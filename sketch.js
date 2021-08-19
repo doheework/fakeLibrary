68f99529bc986c8963636019645ecd711b4ba376
Your Namedo <you@example.com>
2021-08-19 18:51:02 +0900

    sound

--- sketch.js
+++ sketch.js
@@ -20,3 +20,31 @@ $(document).mousemove(function(e){
     $('#shh').css("left", e.pageX);
 
 });
+
+function play() { 
+    var audio = document.getElementById("myaudio"); 
+    if (audio.paused) { 
+        audio.play(); 
+    }else{ 
+        audio.pause(); 
+        audio.currentTime = 0 
+    } 
+} 
+
+$(document).ready(function(){
+    /*웹페이지 열었을 때*/
+    $("#sound").show();
+    $("#mute").hide();
+
+    /*img1을 클릭했을 때 img2를 보여줌*/
+    $("#sound").click(function(){
+        $("#sound").hide();
+        $("#mute").show();
+    });
+
+    /*img2를 클릭했을 때 img1을 보여줌*/
+    $("#mute").click(function(){
+        $("#sound").show();
+        $("#mute").hide();
+    });
+});
